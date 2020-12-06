461c5e97eec4a81555a8b761a6545ad827a56643
Rubén Titos <rtitos@um.es>
2020-12-06 23:24:35 +0100

    Support for building STAMP for x86 and ARM targets

--- common/Defines.common.mk
+++ common/Defines.common.mk
@@ -4,14 +4,31 @@
 #
 # ==============================================================================
 
-#ARCH = { aarch64 , x86_64}
-ARCH      := aarch64
+#ARCH = { aarch64 , x86}
+ARCH      := x86
 
 # Need symlink to gem5 in STAMP root directory to locate handlers/m5 objects
 GEM5_ROOT := $(realpath ../gem5)
 HANDLERS  := $(GEM5_ROOT)/gem5_path/benchmarks/benchmarks-htm/libs/handlers
 
+ifndef ARCH
+$(error ARCH not set)
+else
+$(info $$ARCH is [${ARCH}])
+endif
+# ARCH set via make ARCH=xxx when building this library as part of the
+# build process of STAMP (see common/Defines.common.mk)
+
+ifeq ($(ARCH),aarch64)
+
 GCC_PREFIX := aarch64-linux-gnu-
+
+else ifeq ($(ARCH),x86)
+
+GCC_PREFIX :=
+
+endif
+
 
 CC       := $(GCC_PREFIX)gcc
 CFLAGS   +=  -Wall -pthread
