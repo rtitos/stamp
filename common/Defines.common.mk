b116e8930fff0b2ad5fa27af4ccc710e48941432
Rubén Titos <rtitos@um.es>
2020-12-06 22:58:28 +0100

    ext: Support for building for different target platforms

--- common/Defines.common.mk
+++ common/Defines.common.mk
@@ -4,9 +4,12 @@
 #
 # ==============================================================================
 
-# Symlink to gem5/handlers in benchmarks root dir to locate m5 objects
-GEM5_ROOT = $(realpath ../gem5)
-HANDLERS = $(GEM5_ROOT)/gem5_path/benchmarks/benchmarks-htm/libs/handlers
+#ARCH = { aarch64 , x86_64}
+ARCH      := aarch64
+
+# Need symlink to gem5 in STAMP root directory to locate handlers/m5 objects
+GEM5_ROOT := $(realpath ../gem5)
+HANDLERS  := $(GEM5_ROOT)/gem5_path/benchmarks/benchmarks-htm/libs/handlers
 
 GCC_PREFIX := aarch64-linux-gnu-
 
